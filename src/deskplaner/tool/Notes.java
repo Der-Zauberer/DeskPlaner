8ad80f087c667c783ee3c50d8018f386cc59fda5
Der-Zauberer <and.som282@gmail.com>
2020-05-15 07:57:09 +0200

    Created content for the notes frame

--- src/deskplaner/tool/Notes.java
+++ src/deskplaner/tool/Notes.java
@@ -1,13 +1,19 @@
 package deskplaner.tool;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextArea;
 import deskplaner.util.Tool;
 
 public class Notes extends Tool {
 	
 	private JFrame jframe;
+	private ArrayList<File> files;
 
 	public Notes() {
 		super("Notes");
@@ -15,7 +21,8 @@ public class Notes extends Tool {
 
 	@Override
 	public boolean onCommand(String label, String[] args, File location) {
-		createJFRame();
+		files = new ArrayList<File>();
+		createJFrame();
 		return true;
 	}
 
@@ -24,7 +31,7 @@ public class Notes extends Tool {
 		return "Open the notes window.";
 	}
 	
-	private void createJFRame() {
+	private void createJFrame() {
 		jframe = new JFrame("Notes");
 		jframe.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		jframe.setVisible(true);
@@ -33,10 +40,26 @@ public class Notes extends Tool {
 		jframe.toFront();
 		jframe.requestFocus();
 		jframe.setLayout(new BorderLayout());
+		createJFrameContent();
+	}
+	
+	private void createJFrameContent() {
+		for (int i = 0; i < getFiles().length; i++) {
+			files.add(getFiles()[i]);
+		}
+		String string[] = new String[files.size()];
+		for(int i = 0; i < files.size(); i++) {
+			string[i] = files.get(i).getName().substring(0, files.get(i).getName().length() - 3);
+		}
+		JList<String> jlist = new JList<>(string);
+		jlist.setPreferredSize(new Dimension(200, 100));
+		JTextArea jtextarea = new JTextArea();
+		jframe.getContentPane().add(jlist, BorderLayout.LINE_START);
+		jframe.getContentPane().add(jtextarea, BorderLayout.CENTER);
 	}
 	
 	private File[] getFiles() {
-		return getToolDestination().listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
+		return getToolDestination().listFiles((dir, name) -> name.toLowerCase().endsWith(".md"));
 	}
 	
 }
