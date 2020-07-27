6b0d1235b86eed330fe6d10e04c5acf5ab7f7dd5
Der-Zauberer <and.som282@gmail.com>
2020-07-27 19:42:11 +0200

    Created new layout for tools

---
+++ src/deskplaner/gui/DeskLayout.java
@@ -0,0 +1,40 @@
+package deskplaner.gui;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+public class DeskLayout extends HBox {
+	
+	private VBox vbox = new VBox();
+	private ScrollPane scrollpane = new ScrollPane();
+	private FlowPane flowpane = new FlowPane();
+	
+	public DeskLayout() {
+		this.getChildren().add(new DeskNavigation());
+		this.getChildren().add(vbox);
+		HBox.setHgrow(vbox, Priority.ALWAYS);
+		vbox.getChildren().add(scrollpane);
+		vbox.getStyleClass().add("root");
+		VBox.setVgrow(scrollpane, Priority.ALWAYS);
+		scrollpane.setContent(flowpane);
+		scrollpane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		scrollpane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		scrollpane.setFitToHeight(true);
+		scrollpane.setFitToWidth(true);
+		scrollpane.getStyleClass().add("root");
+		flowpane.getStyleClass().add("root");
+		flowpane.setPadding(new Insets(40));
+		flowpane.setHgap(20);
+		flowpane.setVgap(20);
+	}
+	
+	public FlowPane getFlowPane() {
+		return flowpane;
+	}
+
+}
--- src/deskplaner/tools/Notes.java
+++ src/deskplaner/tools/Notes.java
@@ -3,21 +3,14 @@ package deskplaner.tools;
 import java.io.File;
 import deskplaner.files.FileAssistent;
 import deskplaner.gui.Card;
-import deskplaner.gui.DeskNavigation;
+import deskplaner.gui.DeskLayout;
 import deskplaner.resources.Resource;
 import deskplaner.util.Tool;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 
 public class Notes extends Tool {
 	
-	private static BorderPane borderpane = new BorderPane();
-	private static ScrollPane scrollpane = new ScrollPane();
-	private static FlowPane flowpane = new FlowPane();
+	private static DeskLayout layout = new DeskLayout();
 	
 	public Notes() {
 		super("Notes");
@@ -25,27 +18,14 @@ public class Notes extends Tool {
 	}
 	
 	private void initzializeGui() {
-		this.setScene(new Scene(borderpane));
+		this.setScene(new Scene(layout));
 		this.getScene().getStylesheets().add(Resource.getStyleSheet("style.css"));
-		
-		borderpane.setLeft(new DeskNavigation());
-		borderpane.setCenter(scrollpane);
-		scrollpane.setContent(flowpane);
-		scrollpane.setHbarPolicy(ScrollBarPolicy.NEVER);
-		scrollpane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
-		scrollpane.getStyleClass().add("root");
-		flowpane.prefWidthProperty().bind(scrollpane.widthProperty());
-		flowpane.getStyleClass().add("root");
-		
-		flowpane.setPadding(new Insets(40));
-		flowpane.setHgap(20);
-		flowpane.setVgap(20);
 		
 		for(File file : this.getFiles()) {
 			String name = file.getName().substring(0, file.getName().length() - 4);
 			String content = new FileAssistent(file).readString();
 			Card card = new Card(name, content);
-			flowpane.getChildren().add(card);
+			layout.getFlowPane().getChildren().add(card);
 		}
 	}
 
