702687cff6047f2732327899b411c9bee360b7bf
Der-Zauberer <and.som282@gmail.com>
2020-07-30 18:24:21 +0200

    📦 NEW: YML spacing support

--- src/deskplaner/files/YMLFile.java
+++ src/deskplaner/files/YMLFile.java
@@ -1,6 +1,7 @@
 package deskplaner.files;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,12 +15,18 @@ public class YMLFile {
 	
 	public YMLFile(File file) {
 		this.file = file;
+		FileHandler.createFile(file);
 		String lines[] = FileHandler.readLines(file);
 		if(lines != null && lines.length > 0 && !lines[0].equals("")) {
+			String prefix = "";
 			for (int i = 0; i < lines.length; i++) {
-				String key = lines[i].split(": ", 2)[0];
-				String value = lines[i].split(": ", 2)[1];
-				entries.put(key, value);
+				String key = lines[i].split(":", 2)[0];
+				key = getFullKey(prefix, key);
+				prefix = key;
+				if(lines[i].split(": ", 2).length == 2) {
+					String value = lines[i].split(": ", 2)[1];
+					entries.put(key, value);
+				}
 			}
 		}
 	}
@@ -62,6 +69,36 @@ public class YMLFile {
 			i++;
 		}
 		FileHandler.saveLines(file, lines);
+	}
+	
+	private int getLineSpaces(String string) {
+		int spaces = 0;
+		for(int j = 0; j < string.length(); j++) {
+			if(string.charAt(j) == ' ') {
+				spaces++;
+			} else {
+				break;
+			}
+		}
+		return spaces / 4;
+	}
+	
+	private String getFullKey(String prefix, String key) {
+		String fullkey = "";
+		String keys[];
+		if(prefix.split("\\.").length > 0) {
+			keys = prefix.split("\\.");
+			Arrays.toString(keys);
+		} else {
+			keys = new String[1];
+			keys[0] = prefix;
+		}
+		for(int i = 0; i < getLineSpaces(key); i++) {
+			fullkey += keys[i];
+			if(i < getLineSpaces(key)) fullkey += ".";
+		}
+		fullkey += key.substring(getLineSpaces(key) * 4);
+		return fullkey;
 	}
 
 }
