ee178b03337c4e7caca398224258e046c054b456
Der-Zauberer <and.som282@gmail.com>
2020-08-12 13:04:49 +0200

    📦 NEW: ToolBar for Cards

--- src/deskplaner/gui/Card.java
+++ src/deskplaner/gui/Card.java
@@ -2,17 +2,28 @@ package deskplaner.gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.VBox;
 
 public class Card extends VBox {
 	
+	private VBox top;
+	private VBox bottom;
 	private Label title;
 	private Label text;
+	private ToolBar toolbar;
 	
 	public Card() {
 		this.getStyleClass().add("card");
-		this.setMinWidth(220);
-		this.setPadding(new Insets(20));
+		this.setMinWidth(220);		
+		top = new VBox();
+		this.getChildren().add(top);
+		top.getStyleClass().add("card-top");
+		top.setPadding(new Insets(20));
+		bottom = new VBox();
+		this.getChildren().add(bottom);
+		bottom.getStyleClass().add("card-bottom");
+		
 	}
 	
 	public Card(String title) {
@@ -33,30 +44,12 @@ public class Card extends VBox {
 			} else {
 				this.title = new Label(title);
 				this.title.getStyleClass().add("title");
-				this.getChildren().add(this.title);
+				top.getChildren().add(this.title);
 			}
 		} else {
 			if(this.title != null) {
-				this.getChildren().remove(this.title);
+				top.getChildren().remove(this.title);
 				this.title = null;
-			}
-		}
-	}
-	
-	public void setText(String text) {
-		if(text != null) {
-			if(this.text != null) {
-				this.text.setText(text);
-			} else {
-				this.text = new Label(text);
-				this.getChildren().add(this.text);
-				if(this.title != null) this.title.setPadding(new Insets(0, 0, 20, 0));
-			}
-		} else {
-			if(this.text != null) {
-				this.getChildren().remove(this.text);
-				this.text = null;
-				if(this.title != null) this.title.setPadding(new Insets(0));
 			}
 		}
 	}
@@ -69,12 +62,46 @@ public class Card extends VBox {
 		return title;
 	}
 	
+	public void setText(String text) {
+		if(text != null) {
+			if(this.text != null) {
+				this.text.setText(text);
+			} else {
+				this.text = new Label(text);
+				top.getChildren().add(this.text);
+				if(this.title != null) this.title.setPadding(new Insets(0, 0, 20, 0));
+			}
+		} else {
+			if(this.text != null) {
+				top.getChildren().remove(this.text);
+				this.text = null;
+				if(this.title != null) this.title.setPadding(new Insets(0));
+			}
+		}
+	}
+	
 	public void setText(Label text) {
 		this.text = text;
 	}
 	
 	public Label getText() {
 		return text;
+	}
+	
+	public void initializeToolBar() {
+		toolbar = new ToolBar();
+		bottom.getChildren().add(toolbar);
+		top.getStyleClass().add("top-radius");
+	}
+	
+	public void removeToolBar() {
+		bottom.getChildren().remove(toolbar);
+		top.getStyleClass().remove("top-radius");
+		toolbar = null;
+	}
+	
+	public ToolBar getToolbar() {
+		return toolbar;
 	}
 	
 }
